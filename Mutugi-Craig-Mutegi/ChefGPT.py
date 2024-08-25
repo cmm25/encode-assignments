5ac97430cf67ef88e1dc81c33f16a7f5d3fe7236
cmm25 <craigmutegi@gmail.com>
2024-08-25 16:00:43 +0300

    first commit

---
+++ Mutugi-Craig-Mutegi/ChefGPT.py
@@ -0,0 +1,75 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are an experienced Kenyan chef that helps people by suggesting detailed recipes for dishes they want to cook. You can also provide tips and tricks for cooking and food preparation. You always try to be as clear as possible and provide the best possible recipes for the user's needs. You know a lot about different cuisines and cooking techniques. You are also very short tempered but understanding with the user's needs and questions but brief and sharp in your responses.",
+    }
+]
+messages.append(
+    {
+        "role": "system",
+        "content": "Your client is going to ask for a recipe about a specific dish. If you do not recognize the dish, you should not try to generate a recipe for it. Do not answer a recipe if you do not understand the name of the dish. If you know the dish, you must answer directly with a detailed recipe for it. If you don't know the dish, you should answer that you don't know the dish and end the conversation. in this case the client can : 1 share only ingredients and you should provide 3 dish names and not their recipes. 2. Client can provide a detailed dish name thus a full recipe will be needed. 3 Client can provide a recipe for your critique and suggested improvements.", 
+    }
+)
+
+dish = input("Hi, you may enter ingredients for a dish suggestion, request a dish recipe or maybe a critique for a recipe you want to share:\n")
+messages.append(
+    {
+        "role": "user",
+        "content": f"Suggest me a detailed recipe and the preparation steps for making {dish}"
+    }
+)
+
+model = "gpt-4o-mini"
+
+stream = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True,
+    )
+
+collected_messages = []
+for chunk in stream:
+    chunk_message = chunk.choices[0].delta.content or ""
+    print(chunk_message, end="")
+    collected_messages.append(chunk_message)
+
+messages.append(
+    {
+        "role": "system",
+        "content": "".join(collected_messages)
+    }
+)
+
+while True:
+    print("\n")
+    user_input = input()
+    messages.append(
+        {
+            "role": "user",
+            "content": user_input
+        }
+    )
+    stream = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True,
+    )
+    collected_messages = []
+    for chunk in stream:
+        chunk_message = chunk.choices[0].delta.content or ""
+        print(chunk_message, end="")
+        collected_messages.append(chunk_message)
+    
+    messages.append(
+        {
+            "role": "system",
+            "content": "".join(collected_messages)
+        }
+    )
